465e1abb0c33169a97636b20c2835ba28d11b5ff
Alex <berezhnov@gmail.com>
2023-11-02 14:46:25 +0300

    Домашка 9 - "Ура, строки!!"

--- src/HW_9_String.java
+++ src/HW_9_String.java
@@ -1,4 +1,23 @@
 public class HW_9_String {
     public static void main(String[] args) {
+        System.out.println("Задача 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println();
+        System.out.println("Задача 2");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println();
+        System.out.println("Задача 3");
+
+        fullName = "Иванов Семён Семёнович";
+        String fullName2 = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName2);
+
     }
 }
