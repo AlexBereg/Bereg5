690567ac174313308a1cd29fa1a244e5c2b63ebd
AlexBereg <berezhnov@gmail.com>
2023-12-04 19:33:05 +0300

    Домашка 2-2

---
+++ src/course_2/course_22/Hw_22.java
@@ -0,0 +1,8 @@
+package course_2.course_22;
+
+public class Hw_22 {
+    public static void main(String[] args) {
+        System.out.println("Домашка 22");
+        System.out.println("****************************** ");
+    }
+}
