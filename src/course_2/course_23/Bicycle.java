74e5110ef36c04fbb2d88c8b66476e3248c794cc
AlexBereg <berezhnov@gmail.com>
2023-12-11 18:43:24 +0300

    Домашка 2-3

---
+++ src/course_2/course_23/Bicycle.java
@@ -0,0 +1,10 @@
+package course_2.course_23;
+
+public class Bicycle {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
